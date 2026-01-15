aeda8916001f0960e03bfedf296df2940b6271c8
latriniano <lautarolopezlabrin@gmail.com>
2026-01-15 12:58:36 -0300

    Eliminar middleware temporalmente para solucionar error 500

--- next.config.mjs
+++ next.config.mjs
@@ -5,28 +5,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.googleusercontent.com",
-        pathname: "/**",
-      },
-    ],
-  },
-  // Mantenemos esto para evitar el error de __dirname
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      os: false,
-    };
-    return config;
   },
 };
 
